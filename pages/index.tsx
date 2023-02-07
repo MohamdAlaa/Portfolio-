47f68213f35290084124894a6a015245589b3c9d
Mohamed Alaa <mohamedelmenyawe606@gmail.com>
2023-02-07 13:26:01 +0200

    finished sidebar for now

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,7 @@
 export default function Home() {
   return (
     <>
-    <h1 className="shadow-lg mb-4">Hello world!</h1>
-    <h4 className="shadow-lg ">Hello world!</h4>
+    <h1>bye bye world</h1>
     </>
   )
 }
